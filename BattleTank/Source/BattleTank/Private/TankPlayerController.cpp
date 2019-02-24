67b9a8f9c8a09516c56b63f2d8593b6269503df1
ProskPetr <prosk_petr@protonmail.com>
2019-02-24 20:12:37 +0600

    18 Make BeginPlay() method in TankPlayerController

--- BattleTank/Source/BattleTank/Private/TankPlayerController.cpp
+++ BattleTank/Source/BattleTank/Private/TankPlayerController.cpp
@@ -2,6 +2,22 @@
 
 #include "TankPlayerController.h"
 
+void ATankPlayerController::BeginPlay()
+{
+	Super::BeginPlay();
+
+	auto ControlledTank = GetControlledTank();
+
+	if (!ControlledTank)
+	{	
+		UE_LOG(LogTemp, Error, TEXT("Tank Player Controller doesn't own a Tank!"));
+	}
+	else
+	{
+		UE_LOG(LogTemp, Warning, TEXT("Owned Tank: %s"), *(ControlledTank->GetName()));
+	}
+}
+
 ATank* ATankPlayerController::GetControlledTank() const
 {
 	return Cast<ATank>(GetPawn());
